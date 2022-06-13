38de561a02ce937be717ce0eefc52a06f95c422b
AgustinNeira <104187012+AgustinNeira@users.noreply.github.com>
2022-06-13 01:54:01 -0300

    Actualizacion 9.0
    Practico 9 de Potrero digital, se agrego archivo en JS y funciones nuevas

---
+++ js/javascript.js
@@ -0,0 +1,7 @@
+function nocturno()
+    {
+        document.body.style.backgroundColor = "black";
+    }
+    function pruebafooter() {
+        alert("Creador: Cristian Agustin Neira EMAIL: aguxtin_2x@hotmail.com");
+        }
