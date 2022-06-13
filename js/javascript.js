cd018f4cec19f4b439ccdbb9c3bb5b17407066e4
AgustinNeira <104187012+AgustinNeira@users.noreply.github.com>
2022-06-13 02:04:40 -0300

    Actualizacion 9.1

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,10 @@
 function nocturno()
     {
         document.body.style.backgroundColor = "black";
+        document.getElementById("LI").style.color = "white";
+        document.getElementById("LI2").style.color = "white";
+        document.getElementById("LI3").style.color = "white";
+        document.getElementById("LI4").style.color = "white";
     }
     function pruebafooter() {
         alert("Creador: Cristian Agustin Neira EMAIL: aguxtin_2x@hotmail.com");
